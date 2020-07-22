3c2b0bb6bbbf83882083b7bb038761bd7cebc4cc
gbettega <giovanni.bettega@gmail.com>
2020-07-22 11:16:50 +0200

    Duplicate item now handle correctly the time tag generation

--- simulationnodeclass.h
+++ simulationnodeclass.h
@@ -394,7 +394,7 @@ public:
     bool isSolutionInformation();
 
     //! generate time string
-    std::string SimulationNodeClass::generateTimeString();
+    static std::string generateTimeString();
 };
 
 Q_DECLARE_METATYPE(SimulationNodeClass)
